6a6b8b47f89bebed5e30d1b518ce737f556e0dad
Rolloque, Clark Tj L. <24-33922@g.batstate-u.edu.ph>
2025-11-07 17:20:10 +0700

    feat/list-total-appoinment-owner-maria

--- veterinary.sql
+++ veterinary.sql
@@ -132,3 +132,8 @@ UPDATE doctors SET dlastname = 'Reyes-Gonzales' WHERE dfirstname = 'Sofia' AND d
 SELECT DISTINCT species FROM animals;
 SELECT SUM (totalamount) AS total_sales FROM invoices;
 
+SELECT COUNT(*) AS total_appointments
+FROM appointments a
+JOIN animals an ON a.animalid = an.animalid
+JOIN owners o ON an.ownerid = o.ownerid
+WHERE o.ofirstname = 'Maria';
