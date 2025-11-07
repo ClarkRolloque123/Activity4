ca2d0adc63918ff77c6ec1fddff5cf321e4ce4a5
Clark Tj Rolloque <24-33922@g.batstate-u.edu.ph>
2025-11-07 16:13:49 +0800

    feat/insert-10-rows-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -92,3 +92,11 @@ INSERT INTO appointments (appointid, animalid, appointdate, reason) VALUES
 (8, 8, '2023-04-18', 'Surgery'), 
 (9, 9, '2023-05-03', 'Allergy'), 
 (10, 10, '2023-05-21', 'Eye check');
+
+INSERT INTO doctors (doctorid, dfirstname, dlastname, speciality, phone, email) VALUES
+(1, 'Dr.Maria', 'Santos', 'General Veterinarian', '987-654-3210', 'maria@example.com'),
+(2, 'Dr.Antonio', 'Gonzales', 'Feline Specialist', '555-123-4567', 'antonio@example.com'),
+(3, 'Dr.Felipe', 'Luna', 'Orthopedic Specialist', '111-222-3333', 'felipe@example.com'),
+(4, 'Dr.Sofia', 'Reyes', 'Dermatology Specialist', '999-888-7777', 'sofia@xample.com'),
+(5, 'Dr.Luis', 'Torres', 'Surgery Specialist', '123-555-7777', 'luis@example.com'),
+(6, 'Dr.Carmen', 'Fernandez', 'Opthalmology Specialist', '333-222-1111', 'carmen@example.com');
