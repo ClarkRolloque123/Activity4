d9e9a8cdd77bb169effb1c16a014ae604fc86cad
Clark Tj Rolloque <24-33922@g.batstate-u.edu.ph>
2025-11-07 17:23:24 +0800

    feat/modify-lastname-dr-reyes-gonzales

--- veterinary.sql
+++ veterinary.sql
@@ -128,3 +128,6 @@ INSERT INTO medicalrecords (recordid, animalid, recorddate, doctorid, diagnosis,
 ALTER TABLE owners ADD COLUMN registereddate DATE;
 ALTER TABLE invoices RENAME COLUMN paymentdate TO paymenttime;
 DELETE FROM appointments WHERE animalid = (SELECT animalid FROM animals WHERE name = 'Simba');
+UPDATE doctors SET dlastname = 'Reyes-Gonzales' WHERE dfirstname = 'Sofia' AND dlastname = 'Reyes';
+
+
